249f68e8aaa0ce25b0ca82f19cd6bc64afc1e6e6
Ian Johnson <ian.johnson@almanac.io>
2023-04-11 16:55:47 -0400

    Add comments program

---
+++ Comments.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!\n";				   // std::cout lives in the iostream library
+	std::cout << "It is very nice to meet you!\n"; // these comments make the code hard to read
+	std::cout << "Yeah!\n";						   // especially when lines are different lengths
+
+	// std::cout lives in the iostream library
+	std::cout << "Hello world!\n";
+
+	// this is much easier to read
+	std::cout << "It is very nice to meet you!\n";
+
+	// don't you think so?
+	std::cout << "Yeah!\n";
+
+	/* This is a multi-line comment.
+	 * the matching asterisks to the left
+	 * can make this easier to read
+	 */
+	return 0;
+}
